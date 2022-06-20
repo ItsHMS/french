acd79229fe6bc66adcaa011d306560e9e10e1a37
ItsHMS <hamidmehmood816@gmail.com>
2022-06-20 19:59:18 +0500

    initial commit

---
+++ src/layouts/MainLayout.vue
@@ -0,0 +1,91 @@
+<template>
+  <q-layout view="lHh Lpr lFf">
+    <q-header elevated>
+      <q-toolbar class="text-primary bg-white">
+        <q-avatar square size="100px" font-size="82px">
+          <img alt="Quasar logo" src="~assets/logo.svg" />
+        </q-avatar>
+        <q-toolbar-title
+          class="text-h1"
+          style="
+            font-weight: bold;
+            text-shadow: rgb(0 0 0 / 37%) 1px 4px 4px;
+            font-family: Century Gothic;
+          "
+        >
+          Aziri
+        </q-toolbar-title>
+
+        <q-separator dark vertical />
+        <q-btn
+          stretch
+          href="#notre"
+          flat
+          label="Notre Solution
+En prévention
+"
+        />
+        <q-separator dark vertical />
+        <q-btn
+          stretch
+          flat
+          href="#blanches"
+          label="Salles Blanches
+"
+        />
+        <q-separator dark vertical />
+        <q-btn stretch href="#contact" flat label="Contact" />
+        <q-separator dark vertical />
+        <q-btn stretch flat label="Articles" />
+        <q-separator dark vertical />
+        <q-btn
+          stretch
+          flat
+          label="Se Connecter
+"
+        />
+      </q-toolbar>
+    </q-header>
+
+    <q-page-container>
+      <router-view />
+    </q-page-container>
+  </q-layout>
+</template>
+
+<script>
+const linksList = [
+  {
+    title: "Docs",
+    icon: "school",
+    link: "#",
+  },
+  {
+    title: "Github",
+    icon: "code",
+    link: "#",
+  },
+  {
+    title: "Discord Chat Channel",
+    icon: "chat",
+    link: "#",
+  },
+  {
+    title: "Forum",
+    icon: "record_voice_over",
+    link: "#",
+  },
+  {
+    title: "Twitter",
+    icon: "rss_feed",
+    link: "#",
+  },
+];
+
+export default {
+  name: "MainLayout",
+  data() {
+    return {};
+  },
+};
+</script>
---
+++ src/pages/IndexPage.vue
@@ -0,0 +1,330 @@
+<template>
+  <q-page>
+    <div class="row q-pb-xl">
+      <div class="col-5 self-center items-center text-center text-h3 text-bold">
+        Pour que cela ne se reproduise plus.
+      </div>
+      <q-img class="col-7" src="~assets/Picture1.png" />
+    </div>
+    <!-- 
+
+ -->
+    <div style="background: cornsilk" class="row q-py-xl">
+      <div class="row q-col-gutter-y-md col-8">
+        <div class="col-12 q-mr-md text-h3 text-center text-bold">
+          DE GRAVES CONSÉQUENCES
+        </div>
+        <div class="col-1 row justify-center">
+          <q-avatar>
+            <img src="~assets/Red_Warning.svg" />
+          </q-avatar>
+        </div>
+        <div class="col-10 q-mr-md text-h4 row items-center">
+          Des éclosions mettant en danger les employés et usagers.
+        </div>
+
+        <div class="col-1 row justify-center">
+          <q-avatar>
+            <img src="~assets/Red_Warning.svg" />
+          </q-avatar>
+        </div>
+        <div class="col-10 q-mr-md text-h4 row items-center">
+          La fermeture temporaire de services primordiaux.
+        </div>
+
+        <div class="col-1 row justify-center">
+          <q-avatar>
+            <img src="~assets/Red_Warning.svg" />
+          </q-avatar>
+        </div>
+        <div class="col-10 q-mr-md text-h4 row items-center">
+          La re-planification d'importantes interventions.
+        </div>
+
+        <div class="col-1 row justify-center">
+          <q-avatar>
+            <img src="~assets/Red_Warning.svg" />
+          </q-avatar>
+        </div>
+        <div class="col-10 q-mr-md text-h4 row items-center">
+          Des risques de réputation.
+        </div>
+      </div>
+    </div>
+    <!-- 
+
+     -->
+    <div class="row q-py-xl">
+      <div class="row col-7 q-col-gutter-y-md justify-end">
+        <div class="col-12 q-mr-md text-h3 text-center text-bold">
+          DE GRAVES CONSÉQUENCES
+        </div>
+        <div class="col-1 row items-center">
+          <q-avatar>
+            <img src="~assets/Check_OK.svg" />
+          </q-avatar>
+        </div>
+        <div class="col-10 q-mr-md text-h4 row items-center">
+          Réduction du nombre d’éclosions.
+        </div>
+
+        <div class="col-1 row items-center">
+          <q-avatar>
+            <img src="~assets/Check_OK.svg" />
+          </q-avatar>
+        </div>
+        <div class="col-10 q-mr-md text-h4 row items-center">
+          Meilleure diffusion des bonnes pratiques.
+        </div>
+
+        <div class="col-1 row items-center">
+          <q-avatar>
+            <img src="~assets/Check_OK.svg" />
+          </q-avatar>
+        </div>
+        <div class="col-10 q-mr-md text-h4 row items-center">
+          Réduction des délais liés à l'accessibilité de documentation.
+        </div>
+
+        <div class="col-1 row items-center">
+          <q-avatar>
+            <img src="~assets/Check_OK.svg" />
+          </q-avatar>
+        </div>
+        <div class="col-10 q-mr-md text-h4 row items-center">
+          Réduction des risques financiers.
+        </div>
+
+        <div class="col-1 row items-center">
+          <q-avatar>
+            <img src="~assets/Check_OK.svg" />
+          </q-avatar>
+        </div>
+        <div class="col-10 q-mr-md text-h4 row items-center">
+          Réduction des risques de réputation.
+        </div>
+      </div>
+      <div class="col">
+        <q-img src="~assets/Picture2.png" />
+      </div>
+    </div>
+    <!-- 
+
+     -->
+
+    <div style="background: cornsilk" class="row q-py-xl">
+      <div class="col-12 q-mr-md text-h3 text-center text-bold">
+        LES PERSONNES ET BESOINS VISÉS
+      </div>
+      <div class="col-12 q-mr-md text-h5 text-center">
+        Les agent.es, gestionnaires et techniciens en prévention des
+        établissements de santé. Lors de leurs interventions aimeraient pouvoir
+        :
+      </div>
+      <div
+        style="text-decoration: underline"
+        class="col-12 q-mr-md text-h6 text-light text-center"
+      >
+        Lors de leurs interventions aimeraient pouvoir :
+      </div>
+      <div class="row col-12 justify-center">
+        <div class="col-5">
+          <div class="col-12 row">
+            <div class="col-1 row items-center">
+              <q-avatar>
+                <img src="~assets/Check_OK.svg" />
+              </q-avatar>
+            </div>
+            <div class="col-10 q-mr-md text-h4 row items-center">
+              Réduction des risques financiers.
+            </div>
+          </div>
+          <div class="col">
+            <q-img src="~assets/Picture3.png" />
+          </div>
+        </div>
+        <div class="col-5">
+          <div class="col-12 row">
+            <div class="col-1 row items-center">
+              <q-avatar>
+                <img src="~assets/Check_OK.svg" />
+              </q-avatar>
+            </div>
+            <div class="col-10 q-mr-md text-h4 row items-center">
+              Réduction des risques financiers.
+            </div>
+          </div>
+          <div class="col">
+            <q-img
+              style="max-height: 300px"
+              fit="contain"
+              src="~assets/Picture4.png"
+            />
+          </div>
+        </div>
+      </div>
+    </div>
+
+    <!-- 
+
+     -->
+
+    <div class="row q-py-xl">
+      <div class="col-12 q-mr-md q-pb-xl text-h3 text-center text-bold">
+        2 ZONES D’IMPACTS PRIORITAIRES EN PRÉVENTION
+      </div>
+      <div class="col-12 row q-pb-md">
+        <div class="col-1 row justify-center items-center">
+          <q-avatar>
+            <img src="~assets/arrow_page$.gif" />
+          </q-avatar>
+        </div>
+        <div class="col-8 q-mr-md text-h4 row items-center">
+          L’amélioration des moyens de diffusion de l’information au sein des
+          installations des établissements de santé.
+        </div>
+      </div>
+      <div class="col-12 row">
+        <div class="col-1 row justify-center items-center">
+          <q-avatar>
+            <img src="~assets/logo_page4.gif" />
+          </q-avatar>
+        </div>
+        <div class="col-8 q-mr-md text-h4 row items-center">
+          Les limitations des moyens d’accès rapide à la documentation
+          essentielle en tout temps et de partout.
+        </div>
+      </div>
+    </div>
+
+    <!-- 
+
+
+     -->
+
+    <div id="notre" style="background: cornsilk" class="row q-py-xl">
+      <div class="row col-7 q-col-gutter-y-md justify-end">
+        <div class="col-1 row items-center">
+          <q-avatar>
+            <img src="~assets/Check_OK.svg" />
+          </q-avatar>
+        </div>
+        <div class="col-10 q-mr-md text-h4 row items-center">
+          Une plateforme simple et intuitive.
+        </div>
+
+        <div class="col-1 row items-center">
+          <q-avatar>
+            <img src="~assets/Check_OK.svg" />
+          </q-avatar>
+        </div>
+        <div class="col-10 q-mr-md text-h4 row items-center">
+          Un contrôle personnalisable de la documentation partagées et
+          diffusées.
+        </div>
+
+        <div class="col-1 row items-center">
+          <q-avatar>
+            <img src="~assets/Check_OK.svg" />
+          </q-avatar>
+        </div>
+        <div class="col-10 q-mr-md text-h4 row items-center">
+          Accessible en tout temps et multiplateforme.
+        </div>
+
+        <div class="col-1 row items-center">
+          <q-avatar>
+            <img src="~assets/Check_OK.svg" />
+          </q-avatar>
+        </div>
+        <div class="col-10 q-mr-md text-h4 row items-center">
+          Un portail pour les membres et usagers des établissements.
+        </div>
+      </div>
+      <div class="col">
+        <q-img src="~assets/Picture5.png" />
+      </div>
+    </div>
+
+    <!-- 
+
+     -->
+
+    <div id="blanches" class="row q-py-xl">
+      <div class="row col-7 q-col-gutter-y-md justify-end">
+        <div class="col-1 row items-center">
+          <q-avatar>
+            <img src="~assets/Check_OK.svg" />
+          </q-avatar>
+        </div>
+        <div class="col-10 q-mr-md text-h4 row items-center">
+          Suivi de vos salles à pression positives Salles blanches, Bloc
+          opératoire.
+        </div>
+
+        <div class="col-1 row items-center">
+          <q-avatar>
+            <img src="~assets/Check_OK.svg" />
+          </q-avatar>
+        </div>
+        <div class="col-10 q-mr-md text-h4 row items-center">
+          Suivi de vos salles à pression négative Salles Isolement respiratoire.
+        </div>
+
+        <div class="col-1 row items-center">
+          <q-avatar>
+            <img src="~assets/Check_OK.svg" />
+          </q-avatar>
+        </div>
+        <div class="col-10 q-mr-md text-h4 row items-center">
+          Collaboration avec la prévention et contrôle contre les infections et
+          équipe. des services techniques
+        </div>
+
+        <div class="col-1 row items-center">
+          <q-avatar>
+            <img src="~assets/Check_OK.svg" />
+          </q-avatar>
+        </div>
+        <div class="col-10 q-mr-md text-h4 row items-center">
+          Rapport avec analyse des critères de conformité tel que la
+          température, humidité, le changement d’air à l'heure.
+        </div>
+      </div>
+      <div class="col">
+        <q-img src="~assets/Picture6.png" />
+      </div>
+    </div>
+
+    <!-- 
+
+
+     -->
+
+    <div
+      id="contact"
+      style="background: cornsilk"
+      class="row q-py-xl justify-center"
+    >
+      <div class="row col-4 justify-right">
+        <div class="col-11 text-h6">
+          Il nous fera plaisir de vous en dire plus sur Aziri
+        </div>
+        <div class="col-11 text-h6">
+          DJABRE HABRÉ 514-621-6122 DJABRE.HABRE@AZIRI.CA
+        </div>
+      </div>
+      <div class="col-7">
+        <q-img src="~assets/Picture6.png" />
+      </div>
+    </div>
+  </q-page>
+</template>
+
+<script>
+import { defineComponent } from "vue";
+
+export default defineComponent({
+  name: "IndexPage",
+});
+</script>
