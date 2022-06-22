a6173d732fae870e9c577039c13ffd07edef7a4e
ItsHMS <hamidmehmood816@gmail.com>
2022-06-22 15:23:39 +0500

    changed font-size and heightof section

--- src/pages/IndexPage.vue
+++ src/pages/IndexPage.vue
@@ -1,19 +1,26 @@
 <template>
   <q-page>
-    <div class="row q-pb-xl">
+    <div style="height: calc(100vh - 70px)" class="row">
       <div
-        class="col-12 col-md-5 self-center items-center text-center text-h3 text-bold"
+        style="font-size: 44px"
+        class="col-12 col-md-5 self-center items-center text-start text-h3 text-bold q-px-md"
       >
         Pour que cela ne se reproduise plus.
       </div>
-      <q-img class="col-12 col-md-7" src="~assets/Picture1.png" />
+      <q-img fit="contain" class="col-12 col-md-7" src="~assets/Picture1.png" />
     </div>
     <!-- 
 
  -->
-    <div style="background: cornsilk" class="row q-py-xl">
+    <div
+      style="background: cornsilk; height: calc(100vh - 70px)"
+      class="row q-py-xl"
+    >
       <div class="row q-col-gutter-y-md col-12">
-        <div class="col-12 q-mr-md text-h3 text-center text-bold">
+        <div
+          class="col-12 q-mrl-md text-start q-pl-lg text-bold"
+          style="font-size: 44px"
+        >
           DE GRAVES CONSÉQUENCES
         </div>
         <div class="col-1 row justify-center items-center q-mx-sm">
@@ -21,7 +28,10 @@
             <img src="~assets/Red_Warning.svg" />
           </q-avatar>
         </div>
-        <div class="col-10 q-mr-md text-h4 row items-center">
+        <div
+          class="col-10 q-mr-md text-h4 row items-center"
+          style="font-size: 28px"
+        >
           Des éclosions mettant en danger les employés et usagers.
         </div>
 
@@ -30,7 +40,10 @@
             <img src="~assets/Red_Warning.svg" />
           </q-avatar>
         </div>
-        <div class="col-10 q-mr-md text-h4 row items-center">
+        <div
+          class="col-10 q-mr-md text-h4 row items-center"
+          style="font-size: 28px"
+        >
           La fermeture temporaire de services primordiaux.
         </div>
 
@@ -39,7 +52,10 @@
             <img src="~assets/Red_Warning.svg" />
           </q-avatar>
         </div>
-        <div class="col-10 q-mr-md text-h4 row items-center">
+        <div
+          class="col-10 q-mr-md text-h4 row items-center"
+          style="font-size: 28px"
+        >
           La re-planification d'importantes interventions.
         </div>
 
@@ -48,7 +64,10 @@
             <img src="~assets/Red_Warning.svg" />
           </q-avatar>
         </div>
-        <div class="col-10 q-mr-md text-h4 row items-center">
+        <div
+          class="col-10 q-mr-md text-h4 row items-center"
+          style="font-size: 28px"
+        >
           Des risques de réputation.
         </div>
       </div>
@@ -56,9 +75,12 @@
     <!-- 
 
      -->
-    <div class="row q-py-xl">
+    <div style="height: calc(100vh - 70px)" class="row q-py-xl">
       <div class="row col-12 col-md-7 q-col-gutter-y-md justify-end">
-        <div class="col-12 q-mr-md text-h3 text-center text-bold">
+        <div
+          class="col-12 text-h3 text-start q-pl-lg text-bold"
+          style="font-size: 28px; text-decoration: underline; font-weight: bold"
+        >
           DE GRAVES CONSÉQUENCES
         </div>
         <div class="col-1 row items-center q-mx-sm">
@@ -66,7 +88,10 @@
             <img src="~assets/Check_OK.svg" />
           </q-avatar>
         </div>
-        <div class="col-10 q-mr-md text-h4 row items-center">
+        <div
+          class="col-10 q-mr-md text-h4 row items-center"
+          style="font-size: 28px"
+        >
           Réduction du nombre d’éclosions.
         </div>
 
@@ -75,7 +100,10 @@
             <img src="~assets/Check_OK.svg" />
           </q-avatar>
         </div>
-        <div class="col-10 q-mr-md text-h4 row items-center">
+        <div
+          class="col-10 q-mr-md text-h4 row items-center"
+          style="font-size: 28px"
+        >
           Meilleure diffusion des bonnes pratiques.
         </div>
 
@@ -84,7 +112,10 @@
             <img src="~assets/Check_OK.svg" />
           </q-avatar>
         </div>
-        <div class="col-10 q-mr-md text-h4 row items-center">
+        <div
+          class="col-10 q-mr-md text-h4 row items-center"
+          style="font-size: 28px"
+        >
           Réduction des délais liés à l'accessibilité de documentation.
         </div>
 
@@ -93,7 +124,10 @@
             <img src="~assets/Check_OK.svg" />
           </q-avatar>
         </div>
-        <div class="col-10 q-mr-md text-h4 row items-center">
+        <div
+          class="col-10 q-mr-md text-h4 row items-center"
+          style="font-size: 28px"
+        >
           Réduction des risques financiers.
         </div>
 
@@ -102,7 +136,10 @@
             <img src="~assets/Check_OK.svg" />
           </q-avatar>
         </div>
-        <div class="col-10 q-mr-md text-h4 row items-center">
+        <div
+          class="col-10 q-mr-md text-h4 row items-center"
+          style="font-size: 28px"
+        >
           Réduction des risques de réputation.
         </div>
       </div>
@@ -114,17 +151,23 @@
 
      -->
 
-    <div style="background: cornsilk" class="row q-py-xl">
-      <div class="col-12 q-mr-md text-h3 text-center text-bold">
+    <div
+      style="background: cornsilk; height: calc(100vh - 70px)"
+      class="row q-py-xl"
+    >
+      <div
+        class="col-12 q-mr-md text-start q-pl-lg text-bold"
+        style="font-size: 44px"
+      >
         LES PERSONNES ET BESOINS VISÉS
       </div>
-      <div class="col-12 q-mr-md text-h5 text-center">
+      <div class="col-12 q-mr-md text-start q-pl-lg" style="font-size: 26px">
         Les agent.es, gestionnaires et techniciens en prévention des
         établissements de santé. Lors de leurs interventions aimeraient pouvoir
         :
       </div>
       <div
-        style="text-decoration: underline"
+        style="text-decoration: underline; font-size: 21px"
         class="col-12 q-mr-md text-h6 text-light text-center"
       >
         Lors de leurs interventions aimeraient pouvoir :
@@ -137,8 +180,12 @@
                 <img src="~assets/Check_OK.svg" />
               </q-avatar>
             </div>
-            <div class="col-10 q-mr-md text-h4 row items-center">
-              Réduction des risques financiers.
+            <div
+              class="col-10 q-mr-md text-h4 row items-center"
+              style="font-size: 21px"
+            >
+              <span style="font-weight: bold"> Diffuser</span>&nbsp; de
+              l’information plus efficacement.
             </div>
           </div>
           <div class="col">
@@ -152,8 +199,13 @@
                 <img src="~assets/Check_OK.svg" />
               </q-avatar>
             </div>
-            <div class="col-10 q-mr-md text-h4 row items-center">
-              Réduction des risques financiers.
+            <div
+              class="col-10 q-mr-md text-h4 row items-center"
+              style="font-size: 21px"
+            >
+              <span style="font-weight: bold">Partager</span>&nbsp; ou &nbsp;
+              <span style="font-weight: bold">Consulter</span>&nbsp; de la
+              documentation en tout temps.
             </div>
           </div>
           <div class="col">
@@ -171,31 +223,45 @@
 
      -->
 
-    <div class="row q-py-xl">
-      <div class="col-12 q-mr-md q-pb-xl text-h3 text-center text-bold">
-        2 ZONES D’IMPACTS PRIORITAIRES EN PRÉVENTION
+    <div style="height: calc(100vh - 70px)" class="row q-py-xl">
+      <div class="col-12 col-md-8">
+        <div
+          class="col-12 q-mr-md q-pb-xl text-h3 text-start q-pl-lg text-bold"
+          style="font-size: 40px"
+        >
+          2 ZONES D’IMPACTS PRIORITAIRES EN PRÉVENTION
+        </div>
+        <div class="col-12 row q-pb-md">
+          <div class="col-1 row justify-center items-center q-mx-sm">
+            <q-avatar square>
+              <img src="~assets/arrow_page$.gif" />
+            </q-avatar>
+          </div>
+          <div
+            class="col-10 col-md-8 q-mr-md text-h4 row items-center"
+            style="font-size: 24px; font-weight: bold"
+          >
+            L’amélioration des moyens de diffusion de l’information au sein des
+            installations des établissements de santé.
+          </div>
+        </div>
+        <div class="col-12 row">
+          <div class="col-1 row justify-center items-center q-mx-sm">
+            <q-avatar square>
+              <img src="~assets/logo_page4.gif" />
+            </q-avatar>
+          </div>
+          <div
+            class="col-10 col-md-8 q-mr-md text-h4 row items-center"
+            style="font-size: 24px; font-weight: bold"
+          >
+            Les limitations des moyens d’accès rapide à la documentation
+            essentielle en tout temps et de partout.
+          </div>
+        </div>
       </div>
-      <div class="col-12 row q-pb-md">
-        <div class="col-1 row justify-center items-center q-mx-sm">
-          <q-avatar square>
-            <img src="~assets/arrow_page$.gif" />
-          </q-avatar>
-        </div>
-        <div class="col-10 col-md-8 q-mr-md text-h4 row items-center">
-          L’amélioration des moyens de diffusion de l’information au sein des
-          installations des établissements de santé.
-        </div>
-      </div>
-      <div class="col-12 row">
-        <div class="col-1 row justify-center items-center q-mx-sm">
-          <q-avatar square>
-            <img src="~assets/logo_page4.gif" />
-          </q-avatar>
-        </div>
-        <div class="col-10 col-md-8 q-mr-md text-h4 row items-center">
-          Les limitations des moyens d’accès rapide à la documentation
-          essentielle en tout temps et de partout.
-        </div>
+      <div class="col-12 col-md-4">
+        <q-img src="~assets/Picture4.png" />
       </div>
     </div>
 
@@ -204,14 +270,18 @@
 
      -->
 
-    <div id="notre" style="background: cornsilk" class="row q-py-xl">
+    <div
+      id="notre"
+      style="background: cornsilk; height: calc(100vh - 70px)"
+      class="row q-py-xl"
+    >
       <div class="row col-12 col-md-7 q-col-gutter-y-md justify-end">
         <div class="col-1 row items-center q-mx-sm">
           <q-avatar>
             <img src="~assets/Check_OK.svg" />
           </q-avatar>
         </div>
-        <div class="col-10 q-mr-md text-h4 row items-center">
+        <div class="col-10 q-mr-md row items-center" style="font-size: 18px">
           Une plateforme simple et intuitive.
         </div>
 
@@ -220,7 +290,7 @@
             <img src="~assets/Check_OK.svg" />
           </q-avatar>
         </div>
-        <div class="col-10 q-mr-md text-h4 row items-center">
+        <div class="col-10 q-mr-md row items-center" style="font-size: 18px">
           Un contrôle personnalisable de la documentation partagées et
           diffusées.
         </div>
@@ -230,7 +300,7 @@
             <img src="~assets/Check_OK.svg" />
           </q-avatar>
         </div>
-        <div class="col-10 q-mr-md text-h4 row items-center">
+        <div class="col-10 q-mr-md row items-center" style="font-size: 18px">
           Accessible en tout temps et multiplateforme.
         </div>
 
@@ -239,7 +309,7 @@
             <img src="~assets/Check_OK.svg" />
           </q-avatar>
         </div>
-        <div class="col-10 q-mr-md text-h4 row items-center">
+        <div class="col-10 q-mr-md row items-center" style="font-size: 18px">
           Un portail pour les membres et usagers des établissements.
         </div>
       </div>
@@ -252,14 +322,14 @@
 
      -->
 
-    <div id="blanches" class="row q-py-xl">
-      <div class="row col-12md-7 q-col-gutter-y-md justify-end">
+    <div style="height: calc(100vh - 70px)" id="blanches" class="row q-py-xl">
+      <div class="row col-12 col-md-7 q-col-gutter-y-md justify-end">
         <div class="col-1 row items-center q-mx-sm">
           <q-avatar>
             <img src="~assets/Check_OK.svg" />
           </q-avatar>
         </div>
-        <div class="col-10 q-mr-md text-h4 row items-center">
+        <div class="col-10 q-mr-md row items-center" style="font-size: 18px">
           Suivi de vos salles à pression positives Salles blanches, Bloc
           opératoire.
         </div>
@@ -269,7 +339,7 @@
             <img src="~assets/Check_OK.svg" />
           </q-avatar>
         </div>
-        <div class="col-10 q-mr-md text-h4 row items-center">
+        <div class="col-10 q-mr-md row items-center" style="font-size: 18px">
           Suivi de vos salles à pression négative Salles Isolement respiratoire.
         </div>
 
@@ -278,7 +348,7 @@
             <img src="~assets/Check_OK.svg" />
           </q-avatar>
         </div>
-        <div class="col-10 q-mr-md text-h4 row items-center">
+        <div class="col-10 q-mr-md row items-center" style="font-size: 18px">
           Collaboration avec la prévention et contrôle contre les infections et
           équipe. des services techniques
         </div>
@@ -288,7 +358,7 @@
             <img src="~assets/Check_OK.svg" />
           </q-avatar>
         </div>
-        <div class="col-10 q-mr-md text-h4 row items-center">
+        <div class="col-10 q-mr-md row items-center" style="font-size: 18px">
           Rapport avec analyse des critères de conformité tel que la
           température, humidité, le changement d’air à l'heure.
         </div>
@@ -305,19 +375,22 @@
 
     <div
       id="contact"
-      style="background: cornsilk"
-      class="row q-py-xl justify-center"
+      style="background: cornsilk; height: calc(100vh - 70px)"
+      class="row justify-center"
     >
       <div class="row col-4 justify-right">
-        <div class="col-11 text-h6">
+        <div class="col-11" style="font-size: 26px">
           Il nous fera plaisir de vous en dire plus sur Aziri
         </div>
-        <div class="col-11 text-h6">
+        <div class="col-11" style="font-size: 26px">
           DJABRE HABRÉ 514-621-6122 DJABRE.HABRE@AZIRI.CA
         </div>
+        <div class="col-11" style="font-size: 44px; font-weight: bold">
+          MERCI !
+        </div>
       </div>
-      <div class="col-7">
-        <q-img src="~assets/Picture6.png" />
+      <div class="col-7 bgIMG">
+        <q-img src="~assets/Picture7.png" fit="contain" :ratio="4 / 3" />
       </div>
     </div>
   </q-page>
@@ -330,3 +403,10 @@ export default defineComponent({
   name: "IndexPage",
 });
 </script>
+<style scoped>
+.bgIMG {
+  background-image: url("~assets/bg8.png");
+  background-repeat: no-repeat;
+  background-size: contain;
+}
+</style>
